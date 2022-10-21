2c1d159b60c7a627ff9975de26a625f40bf0be8f
JulietShaban <julietshaban@gmail.com>
2022-10-21 14:17:36 +0300

    Working on a CookBook prjct

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ButtonBases from "./ButtonBases";
+import {Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="app-name" >Pocket CookBook</h1>
+      </header>
+      <ButtonBases/>
+      </div>
+  );
+}
+
+export default App;
